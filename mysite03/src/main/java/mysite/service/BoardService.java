bf1565dcac48bc54b8be643b6949b0ca1fb1debe
kilho <kilho@localhost>
2024-12-27 16:49:47 +0900

    Guestbook spring #1

--- mysite03/src/main/java/mysite/service/BoardService.java
+++ mysite03/src/main/java/mysite/service/BoardService.java
@@ -5,25 +5,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class BoardService {
 	
-	public void addContents(BoardVo vo) {
-	}
-	
-	public BoardVo getContents(long id) {
-	}
-	
-	public BoardVo getContents(long id, Long userId) {
-	}
-	
-	public void updateContents(BoardVo vo) {
-	}
-	
-	public void eleteContents(Long id, Long userId) {
-	}
-	
-	public Map<String, Object> getContentsList(int currentage, String keyword){
-		List<BoardVo> list = null;
-		// view의 pagination를 위한 데이터 값 계산
-	}
-	
+//	public void addContents(BoardVo vo) {
+//	}
+//	
+//	public BoardVo getContents(long id) {
+//	}
+//	
+//	public BoardVo getContents(long id, Long userId) {
+//	}
+//	
+//	public void updateContents(BoardVo vo) {
+//	}
+//	
+//	public void eleteContents(Long id, Long userId) {
+//	}
+//	
+//	public Map<String, Object> getContentsList(int currentage, String keyword){
+//		List<BoardVo> list = null;
+//		// view의 pagination를 위한 데이터 값 계산
+//	}
+//	
 	
 }
