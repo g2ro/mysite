c85a1f4916c7675663c4085caf039db60e0d85c7
kilho <kilho@localhost>
2025-01-02 14:30:25 +0900

    exception #finish

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -3,20 +3,26 @@ package mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log Log = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String handler(Exception e) {
+	public String handler(Model model,Exception e) {
 		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		System.out.println(errors.toString());
-			
+		Log.error(errors.toString());
+	
 		// 2. 사과 페이
+		model.addAttribute("errors", errors.toString());
 		return "errors/exception";
 	}
 }
