c76f47b3dcd66868733ff329b6be9c949a2741b4
kilho <kilho@localhost>
2025-01-03 14:53:07 +0900

    prepare fileupload

---
+++ mysite03/src/main/java/mysite/service/SiteService.java
@@ -0,0 +1,15 @@
+package mysite.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SiteService {
+	
+//	public SiteVo getSite() {
+//		return null;
+//	}
+//	
+//	public void updateSite(SiteVo siteVo) {
+//		
+//	}
+}
