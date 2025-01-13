a511260e717142e2bbfa5465d3b20fdb1a51e576
g2ro <kilho603@gmail.com>
2025-01-13 13:47:29 +0900

    mysite04 -java config#2

--- mysite04/src/main/java/mysite/config/app/DBConfig.java
+++ mysite04/src/main/java/mysite/config/app/DBConfig.java
@@ -3,21 +3,28 @@ package mysite.config.app;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionManager;
 
 @Configuration
+@PropertySource("classpath:mysite/config/app/jdbc.properties")
 public class DBConfig {
+	
+	@Autowired
+	private Environment env;
 	
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-		dataSource.setUrl("jdbc:mariadb://192.168.64.3:3306/webdb");
-		dataSource.setUsername("webdb");
-		dataSource.setPassword("webdb");
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
 		
 		return dataSource;
 	}
