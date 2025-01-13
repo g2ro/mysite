26f883fc3d07c3dd876861525d52d1fde46f00ed
g2ro <kilho603@gmail.com>
2025-01-13 10:50:28 +0900

    Exception Handler 수정

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -2,27 +2,47 @@ package mysite.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mysite.dto.JsonResult;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log Log = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String handler(Model model,Exception e) {
+	public void handler(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Exception e) throws Exception{
 		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		System.out.println(errors.toString());
 		Log.error(errors.toString());
 	
-		// 2. 사과 페이
-		model.addAttribute("errors", errors.toString());
-		return "errors/exception";
+		// 2. 요청 구분
+		// 	json 요청: request header의 accept: application/json (o)
+		// 	html 요청: request header의 accept: application/json (x) : json 요청에는 html이 들어갈 수 있지만, html에는 절대로 json 이 들어갈 수 없음.
+		String accept = request.getHeader("accept");
+		if(accept.matches(".*application/json.*")){ // matches내부에는 정규표현식
+			// 3. json 응답
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			
+		} else {
+			// 4. 사과 페이지 (종료)
+			request.setAttribute("errors", errors.toString());
+			request
+				.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp")
+				.forward(request, response);
+		}
+		
 	}
 }
