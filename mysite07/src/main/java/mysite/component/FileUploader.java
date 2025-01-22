4c71558dcc2a2d966c26aa673511c34a1aa388a2
g2ro <kilho603@gmail.com>
2025-01-22 15:51:03 +0900

    [mysite06,07] hotfix fileuploader error

--- mysite07/src/main/java/mysite/component/FileUploader.java
+++ mysite07/src/main/java/mysite/component/FileUploader.java
@@ -7,16 +7,25 @@ import java.io.OutputStream;
 import java.util.Calendar;
 import java.util.Optional;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Component
+@Service
+@PropertySource("classpath:config/fileupload.properties")
 public class FileUploader {
-	private static final String SAVE_PATH = "/Users/kilho/mysite-uploads";
-	private static final String URL = "/assets/upload-images";
+	@Autowired
+	private Environment env;
+	
+//	private static final String SAVE_PATH = "/Users/kilho/mysite-uploads";
+//	private static final String URL = "/assets/upload-images";
+	
 	public String restore(MultipartFile file) throws RuntimeException{
+		
 		try {
-			File uploadDirectory = new File(SAVE_PATH);
+			File uploadDirectory = new File(env.getProperty("fileupload.uploadLocation"));
 			if(!uploadDirectory.exists() && !uploadDirectory.mkdirs()) {
 				return null;
 			}
@@ -30,19 +39,22 @@ public class FileUploader {
 			String saveFilename = generateSaveFilename(extName);
 			long fileSize = file.getSize();
 			
-		
+			System.out.println("########" + originFilename);
+			System.out.println("########" + saveFilename);
+			System.out.println("########" + fileSize);
+			
 			byte[] data = file.getBytes();
 			
-			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
+			OutputStream os = new FileOutputStream(env.getProperty("fileupload.uploadLocation") + "/" +saveFilename);
 			os.write(data);
 			os.close();
-			return URL + "/" + saveFilename;
-		} catch (IOException e) {
+			
+			return env.getProperty("fileupload.resourceUrl") + "/" + saveFilename;
+		}catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
 	}
-	
+
 	private String generateSaveFilename(String extName) {
 		Calendar calendar = Calendar.getInstance();
 		
@@ -56,4 +68,5 @@ public class FileUploader {
 			+ calendar.get(Calendar.MILLISECOND)
 			+ ("." + extName);
 	}
+	
 }
